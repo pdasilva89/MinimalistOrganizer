527d37f30f6fbc86ea7050210e2792a4ad971ea6
Priscila DaSilva <pdasilva89@outlook.com>
2020-02-27 22:30:07 -0800

    Update validation.js

--- contact/validation.js
+++ contact/validation.js
@@ -1,20 +1,4 @@
-
-function validation () {
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var email = document.getElementById("email").value;
-
-  if(firstName === "") {
-    return window.alert("Please enter your first name");
-  }
-  if (lastName === "") {
-    return window.alert("Please enter your last name ")
-  }
-  if (email === "" || email.includes("@") !== true) {
-    return window.alert("Please enter your email address ")
-  }
-}
-
-document.getElementById("button").onclick = function () {
-  validation();
-};
+function validation(){var firstName=document.getElementById("firstName").value;var lastName=document.getElementById("lastName").value;var email=document.getElementById("email").value;if(firstName===""){return window.alert("Please enter your first name")}
+if(lastName===""){return window.alert("Please enter your last name ")}
+if(email===""||email.includes("@")!==!0){return window.alert("Please enter your email address ")}}
+document.getElementById("button").onclick=function(){validation()}
